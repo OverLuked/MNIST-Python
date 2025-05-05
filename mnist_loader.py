20cd3c52e5ceb4aec3413e10e42c67d61e0ebe6d
OverLuked <edillorluke@gmail.com>
2025-05-06 03:33:09 +0800

    Fix data path

--- mnist_loader.py
+++ mnist_loader.py
@@ -3,7 +3,7 @@ import gzip
 import numpy as np
 
 def load_data():
-    f = gzip.open('mnist.pkl.gz', 'rb')
+    f = gzip.open('neural-networks-and-deep-learning/data/mnist.pkl.gz', 'rb')
     training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
     f.close()
     return (training_data, validation_data, test_data)
